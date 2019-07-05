3a45711cb0431621b18b06fc4950741ea664c002
Andrew Lima <andrew@arctek.co.za>
2019-07-05 16:13:09 +0200

    Escape data inside email class.

--- classes/class.approvalemails.php
+++ classes/class.approvalemails.php
@@ -26,23 +26,23 @@ class PMPro_Approvals_Email extends PMProEmail {
 
 		$level = pmpro_getMembershipLevelForUser( $member->ID );
 
-		$this->email    = $member->user_email;
+		$this->email    = sanitize_email( $member->user_email );
 		$this->subject  = sprintf( __( 'Your membership at %s has been approved.', 'pmpro-approvals' ), get_bloginfo( 'name' ) );
 		$this->template = 'application_approved';
 		$this->body     = file_get_contents( PMPRO_APP_DIR . '/email/application_approved.html' );
 		$this->data     = array(
-			'subject'               => $this->subject,
-			'name'                  => $member->display_name,
-			'member_email'          => $member->user_email,
-			'user_login'            => $member->user_login,
+			'subject'               => esc_attr( $this->subject ),
+			'name'                  => esc_attr( $member->display_name ),
+			'member_email'          => sanitize_email( $member->user_email ),
+			'user_login'            => esc_attr( $member->user_login ),
 			'sitename'              => get_option( 'blogname' ),
-			'membership_id'         => $level->id,
-			'membership_level_name' => $level->name,
-			'siteemail'             => pmpro_getOption( 'from_email' ),
+			'membership_id'         => intval( $level->id ),
+			'membership_level_name' => esc_attr( $level->name ),
+			'siteemail'             => sanitize_email( pmpro_getOption( 'from_email' ) ),
 			'login_link'            => wp_login_url(),
 		);
-		$this->from     = pmpro_getOption( 'from' );
-		$this->fromname = pmpro_getOption( 'from_name' );
+		$this->from     = esc_attr( pmpro_getOption( 'from' ) );
+		$this->fromname = esc_attr( pmpro_getOption( 'from_name' ) );
 
 		$this->data = apply_filters( 'pmpro_approvals_member_approved_email_data', $this->data, $member, $level );
 
@@ -64,23 +64,23 @@ class PMPro_Approvals_Email extends PMProEmail {
 
 		$level = pmpro_getMembershipLevelForUser( $member->ID );
 
-		$this->email    = $member->user_email;
+		$this->email    = sanitize_email( $member->user_email );
 		$this->subject  = sprintf( __( 'Your membership at %s has been denied.', 'pmpro-approvals' ), get_bloginfo( 'name' ) );
 		$this->template = 'application_denied';
 		$this->body     = file_get_contents( PMPRO_APP_DIR . '/email/application_denied.html' );
 		$this->data     = array(
-			'subject'               => $this->subject,
-			'name'                  => $member->display_name,
-			'member_email'          => $member->user_email,
-			'user_login'            => $member->user_login,
-			'sitename'              => get_option( 'blogname' ),
-			'membership_id'         => $level->id,
-			'membership_level_name' => $level->name,
-			'siteemail'             => pmpro_getOption( 'from_email' ),
+			'subject'               => esc_attr( $this->subject ),
+			'name'                  => esc_attr( $member->display_name ),
+			'member_email'          => sanitize_email( $member->user_email ),
+			'user_login'            => esc_attr( $member->user_login ),
+			'sitename'              => esc_attr( get_option( 'blogname' ) ),
+			'membership_id'         => intval( $level->id ),
+			'membership_level_name' => esc_attr( $level->name ),
+			'siteemail'             => sanitize_email( pmpro_getOption( 'from_email' ) ),
 			'login_link'            => wp_login_url(),
 		);
-		$this->from     = pmpro_getOption( 'from' );
-		$this->fromname = pmpro_getOption( 'from_name' );
+		$this->from     = esc_attr( pmpro_getOption( 'from' ) );
+		$this->fromname = esc_attr( pmpro_getOption( 'from_name' ) );
 
 		$this->data = apply_filters( 'pmpro_approvals_member_denied_email_data', $this->data, $member );
 
@@ -102,20 +102,20 @@ class PMPro_Approvals_Email extends PMProEmail {
 			$admin = get_user_by( 'ID', $admin );
 		}
 
-		$this->email    = get_bloginfo( 'admin_email' );
+		$this->email    = sanitize_email( get_bloginfo( 'admin_email' ) );
 		$this->subject  = sprintf( __( 'A member at %s is waiting approval.', 'pmpro-approvals' ), get_bloginfo( 'name' ) );
 		$this->template = 'admin_notification_approval';
 		$this->body     = file_get_contents( PMPRO_APP_DIR . '/email/admin_notification.html' );
 		$this->data     = array(
-			'subject'               => $this->subject,
-			'name'                  => $admin->display_name,
-			'user_login'            => $admin->user_login,
-			'sitename'              => get_option( 'blogname' ),
-			'siteemail'             => pmpro_getOption( 'from_email' ),
+			'subject'               => esc_attr( $this->subject ),
+			'name'                  => esc_attr( $admin->display_name ),
+			'user_login'            => esc_attr( $admin->user_login )
+			'sitename'              => esc_attr( get_option( 'blogname' ) ),
+			'siteemail'             => sanitize_email( pmpro_getOption( 'from_email' ) ),
 			'login_link'            => wp_login_url(),
 		);
-		$this->from     = pmpro_getOption( 'from' );
-		$this->fromname = pmpro_getOption( 'from_name' );
+		$this->from     = esc_attr( pmpro_getOption( 'from' ) );
+		$this->fromname = esc_attr( pmpro_getOption( 'from_name' ) );
 
 		if ( ! empty( $member ) ) {
 
@@ -127,13 +127,13 @@ class PMPro_Approvals_Email extends PMProEmail {
 
 			$level = pmpro_getMembershipLevelForUser( $member->ID );
 
-			$this->data['member_name']  = $member->display_name;
-			$this->data['member_email'] = $member->user_email;
-			$this->data['membership_id']         = $level->id;
-			$this->data['membership_level_name'] = $level->name;
-			$this->data['view_profile'] = admin_url( 'admin.php/?page=pmpro-approvals&user_id=' . $member->ID );
-			$this->data['approve_link'] = $this->data['view_profile'] . '&approve=' . $member->ID;
-			$this->data['deny_link']    = $this->data['view_profile'] . '&deny=' . $member->ID;
+			$this->data['member_name']  = esc_attr( $member->display_name );
+			$this->data['member_email'] = esc_attr( $member->user_email );
+			$this->data['membership_id']         = intval( $level->id );
+			$this->data['membership_level_name'] = esc_attr( $level->name );
+			$this->data['view_profile'] = admin_url( 'admin.php/?page=pmpro-approvals&user_id=' . intval( $member->ID ) );
+			$this->data['approve_link'] = $this->data['view_profile'] . '&approve=' . intval( $member->ID );
+			$this->data['deny_link']    = $this->data['view_profile'] . '&deny=' . intval( $member->ID );
 		}
 
 		$this->data = apply_filters( 'pmpro_approvals_admin_pending_email_data', $this->data, $member, $admin );
@@ -155,20 +155,20 @@ class PMPro_Approvals_Email extends PMProEmail {
 			$admin = get_user_by( 'ID', $admin );
 		}
 
-		$this->email    = get_bloginfo( 'admin_email' );
+		$this->email    = sanitize_email( get_bloginfo( 'admin_email' ) );
 		$this->subject  = sprintf( __( 'A member at %s has been approved.', 'pmpro-approvals' ), get_bloginfo( 'name' ) );
 		$this->template = 'admin_approved';
 		$this->body     = file_get_contents( PMPRO_APP_DIR . '/email/admin_approved.html' );
 		$this->data     = array(
-			'subject'    => $this->subject,
-			'name'       => $admin->display_name,
-			'user_login' => $admin->user_login,
-			'sitename'   => get_option( 'blogname' ),
-			'siteemail'  => pmpro_getOption( 'from_email' ),
+			'subject'    => esc_attr( $this->subject ),
+			'name'       => esc_attr( $admin->display_name ),
+			'user_login' => esc_attr( $admin->user_login ),
+			'sitename'   => esc_attr( get_option( 'blogname' ) ),
+			'siteemail'  => sanitize_email( pmpro_getOption( 'from_email' ) ),
 			'login_link' => wp_login_url(),
 		);
-		$this->from     = pmpro_getOption( 'from' );
-		$this->fromname = pmpro_getOption( 'from_name' );
+		$this->from     = esc_attr( pmpro_getOption( 'from' ) );
+		$this->fromname = esc_attr( pmpro_getOption( 'from_name' ) );
 
 		// Let's add in the user approval data if it's available.
 		if ( ! empty( $member ) ) {
@@ -181,11 +181,11 @@ class PMPro_Approvals_Email extends PMProEmail {
 
 			$level = pmpro_getMembershipLevelForUser( $member->ID );
 
-			$this->data['membership_id']         = $level->id;
-			$this->data['membership_level_name'] = $level->name;
-			$this->data['member_email']          = $member->user_email;
-			$this->data['member_name']           = $member->display_name;
-			$this->data['view_profile']          = admin_url( 'admin.php/?page=pmpro-approvals&user_id=' . $member->ID );
+			$this->data['membership_id']         = intval( $level->id );
+			$this->data['membership_level_name'] = esc_attr( $level->name );
+			$this->data['member_email']          = sanitize_email( $member->user_email );
+			$this->data['member_name']           = esc_attr( $member->display_name );
+			$this->data['view_profile']          = admin_url( 'admin.php/?page=pmpro-approvals&user_id=' . intval( $member->ID ) );
 		}
 
 		$this->data = apply_filters( 'pmpro_approvals_admin_approved_email_data', $this->data, $member, $admin );
@@ -207,20 +207,20 @@ class PMPro_Approvals_Email extends PMProEmail {
 			$admin = get_user_by( 'ID', $admin );
 		}
 
-		$this->email    = get_bloginfo( 'admin_email' );
+		$this->email    = sanitize_email( get_bloginfo( 'admin_email' ) );
 		$this->subject  = sprintf( __( 'A member at %s has been denied.', 'pmpro-approvals' ), get_bloginfo( 'name' ) );
 		$this->template = 'admin_denied';
 		$this->body     = file_get_contents( PMPRO_APP_DIR . '/email/admin_denied.html' );
 		$this->data     = array(
-			'subject'    => $this->subject,
-			'name'       => $admin->display_name,
-			'user_login' => $admin->user_login,
-			'sitename'   => get_option( 'blogname' ),
-			'siteemail'  => pmpro_getOption( 'from_email' ),
+			'subject'    => esc_attr( $this->subject ),
+			'name'       => esc_attr( $admin->display_name ),
+			'user_login' => esc_attr( $admin->user_login ),
+			'sitename'   => esc_attr( get_option( 'blogname' ) ),
+			'siteemail'  => sanitize_email( pmpro_getOption( 'from_email' ) ),
 			'login_link' => wp_login_url(),
 		);
-		$this->from     = pmpro_getOption( 'from' );
-		$this->fromname = pmpro_getOption( 'from_name' );
+		$this->from     = esc_attr( pmpro_getOption( 'from' ) );
+		$this->fromname = esc_attr( pmpro_getOption( 'from_name' ) );
 
 		// Let's add in the user approval data if it's available.
 		if ( ! empty( $member ) ) {
@@ -233,10 +233,10 @@ class PMPro_Approvals_Email extends PMProEmail {
 
 			$level = pmpro_getMembershipLevelForUser( $member->ID );
 
-			$this->data['membership_id']         = $level->id;
-			$this->data['membership_level_name'] = $level->name;
-			$this->data['member_email']          = $member->user_email;
-			$this->data['member_name']           = $member->display_name;
+			$this->data['membership_id']         = intval( $level->id );
+			$this->data['membership_level_name'] = esc_attr( $level->name );
+			$this->data['member_email']          = esc_attr( $member->user_email );
+			$this->data['member_name']           = esc_attr( $member->display_name );
 			$this->data['view_profile']          = admin_url( 'admin.php/?page=pmpro-approvals&user_id=' . $member->ID );
 		}
 
